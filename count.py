f8fd81e9de67e544a9e7f1266c7c430297edf807
Mac <mac@WYs-MacBook-Pro.local>
2020-05-25 14:57:43 -0500

    script

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 fname = "README.md"
 count = 0
-with open(fname, 'r') as f:
-    for line in f:
-        count += 1
-
+file_1 = open(fname, 'r')
+for line in file_1:
+    count += 1
+file_1.close()
 print("total lines in README: ", count)
